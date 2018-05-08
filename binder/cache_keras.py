ceb2788562b52b46ed58db440d3638ee727736bd
Kevin Mader <kevinmader@gmail.com>
2018-05-08 19:19:18 +0200

    fixing install commands

--- binder/cache_keras.py
+++ binder/cache_keras.py
@@ -6,11 +6,6 @@ import keras.applications as apps
 for c_app in dir(apps):
     try:
         # initialize the model
-        apps.__getattribute__(c_app)()
-    except Exception as e:
-        print('{} complained that {}'.format(c_app, e))
-    try:
-        # initialize the model
         apps.__getattribute__(c_app)(include_top = False)
     except Exception as e:
         print('{} complained that {}'.format(c_app, e))
